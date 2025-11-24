652d9abcb3764e26cd85c9fd1accc55f89a17eca
David Faux <david.faux@aax.co.za>
2025-11-24 09:00:55 +0200

    Update Vite configuration to set base path for deployment
    - Added a base path of '/arc-raiders-solo-planner/' in vite.config.ts to ensure correct asset loading during deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
   import path from 'path';
 
   export default defineConfig({
+    base: '/arc-raiders-solo-planner/'
     plugins: [react(), tailwindcss()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
